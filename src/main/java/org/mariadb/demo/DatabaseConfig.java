08c871b749acc41f66d88ec3f396444009cc5714
rusher <diego.dupin@gmail.com>
2017-10-20 12:36:51 +0200

    micro example

---
+++ src/main/java/org/mariadb/demo/DatabaseConfig.java
@@ -0,0 +1,37 @@
+package org.mariadb.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+    @Value("${db.driver}")
+    private String DB_DRIVER;
+
+    @Value("${db.password}")
+    private String DB_PASSWORD;
+
+    @Value("${db.url}")
+    private String DB_URL;
+
+    @Value("${db.username}")
+    private String DB_USERNAME;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(DB_DRIVER);
+        dataSource.setUrl(DB_URL);
+        dataSource.setUsername(DB_USERNAME);
+        dataSource.setPassword(DB_PASSWORD);
+        return dataSource;
+    }
+
+}
